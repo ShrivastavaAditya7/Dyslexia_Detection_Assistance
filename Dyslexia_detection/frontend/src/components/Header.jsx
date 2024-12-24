ad354c931f354143a8faf9cb951189c5c96b0668
dhruv jain <dhruvdkjk@gmail.com>
2024-12-24 15:17:19 +0530

    frontend UI completed

--- Dyslexia_detection/frontend/src/components/Header.jsx
+++ Dyslexia_detection/frontend/src/components/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Switch from './Switch';
 
-const Header = () => {
+const Header = ({ setIsDarkMode }) => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className=" bg-blue-100 dark:bg-gray-800 p-6  border-b-2 border-gray-300 flex justify-between items-start">
+      <div className='md:ml-20 md:mt-10'>
+        <h1 className="text-3xl md:text-4xl text-gray-800 dark:text-white font-bold text-left">Early Dyslexia Detection</h1>
+        <p className="text-base md:text-lg text-gray-600 dark:text-gray-500 mt-2 text-left">Unlocking Potential for Lifelong Success</p>
+      </div>
 
-export default Header
+      <div className='mt-10 md:mr-10'>
+        <Switch setIsDarkMode={setIsDarkMode} /> 
+      </div>
+    </header>
+  );
+};
+
+export default Header;
